044e601dabf0d9e9f1ab441252206b99bf44ae4b
Devyaterikov Petr <p.devyaterkov@aoamt.ru>
2021-12-24 20:46:09 +0300

    updates config

--- install.sh
+++ install.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 uninstall () {
-  rm -rf ~/.vimrc ~/.vim ~/.tmux.conf
+  rm -rf ~/.vimrc ~/.vim ~/.tmux.conf ~/.config/nvim/init.vim
 }
 
 install () {
-  if [ -f ~/.vimrc ] || [ -f ~/.tmux.conf ]; then
+  if [ -f ~/.vimrc ] || [ -f ~/.tmux.conf ] || [ -f ~/.config/nvim/init.vim ]; then
     echo 'Config is exist!'
     exit
   else
     ROOT_PATH=$(dirname $(readlink -e $0))
     ln -s "$ROOT_PATH/.vimrc" ~/.vimrc
     ln -s "$ROOT_PATH/.tmux.conf" ~/.tmux.conf
-    vim +PlugInstall +qall
+    mkdir -p ~/.config/nvim
+    ln -s "$ROOT_PATH/init.vim" ~/.config/nvim/init.vim
+    ln -s "$ROOT_PATH/coc-settings.json" ~/.config/nvim/coc-settings.json
+    nvim +PlugInstall +qall
+    tmux
   fi
 }
 
