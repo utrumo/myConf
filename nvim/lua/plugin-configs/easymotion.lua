e681c319b0e1150201cec41d00d3d315b11f0437
Petr Devyaterikov <utrumo@gmail.com>
2022-11-06 17:13:37 +0300

    Corrects easymotion config

--- nvim/lua/plugin-configs/easymotion.lua
+++ nvim/lua/plugin-configs/easymotion.lua
@@ -1,5 +1,3 @@
 local keymap = vim.api.nvim_set_keymap
 
-vim.g.EasyMotion_smartcase = 1
-
-keymap('n', '<Leader>s', '<Plug>(easymotion-overwin-f2)', {})
+keymap('n', '<Leader>s', '<Plug>(easymotion-overwin-f)', {})
