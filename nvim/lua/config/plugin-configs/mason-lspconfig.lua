761fff1bf1a455546e98ae2ea17628359f5e4d37
Petr Devyaterikov <utrumo@gmail.com>
2023-04-15 23:51:46 +0300

    Shows diagnostic on cursor hold

--- nvim/lua/config/plugin-configs/mason-lspconfig.lua
+++ nvim/lua/config/plugin-configs/mason-lspconfig.lua
@@ -1,15 +1,37 @@
-require('mason-lspconfig').setup({
-  ensure_installed = { 'vimls', 'lua_ls', 'tsserver' },
-})
+local masonLspConfig = require('mason-lspconfig')
+local lspConfig = require('lspconfig')
 
-require('mason-lspconfig').setup_handlers({
-  -- The first entry (without a key) will be the default handler
-  -- and will be called for each installed server that doesn't have
-  -- a dedicated handler.
-  function(server_name) -- default handler (optional)
-    require('lspconfig')[server_name].setup({})
-  end,
-  -- Next, you can provide a dedicated handler for specific servers.
-  -- For example, a handler override for the `rust_analyzer`:
-  -- ['rust_analyzer'] = function() require('rust-tools').setup({}) end,
-})
+local on_attach = function(client, bufnr)
+  vim.api.nvim_create_autocmd('CursorHold', {
+    buffer = bufnr,
+    callback = function()
+      local opts = {
+        focusable = false,
+        close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
+        border = 'rounded',
+        source = 'always',
+        prefix = ' ',
+        scope = 'cursor',
+      }
+      vim.diagnostic.open_float(nil, opts)
+    end,
+  })
+end
+
+masonLspConfig
+  .setup({
+    ensure_installed = { 'vimls', 'lua_ls', 'tsserver' },
+  })
+  .setup_handlers({
+    -- The first entry (without a key) will be the default handler
+    -- and will be called for each installed server that doesn't have
+    -- a dedicated handler.
+    function(server_name) -- default handler (optional)
+      lspConfig[server_name].setup({
+        on_attach = on_attach,
+      })
+    end,
+    -- Next, you can provide a dedicated handler for specific servers.
+    -- For example, a handler override for the `rust_analyzer`:
+    -- ['rust_analyzer'] = function() require('rust-tools').setup({}) end,
+  })
