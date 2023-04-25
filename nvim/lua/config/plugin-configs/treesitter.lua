d5acb8173e4481d9a82616f76c28c991a76cce15
Petr Devyaterikov <utrumo@gmail.com>
2023-04-25 12:38:58 +0300

    Enable folding

--- nvim/lua/config/plugin-configs/treesitter.lua
+++ nvim/lua/config/plugin-configs/treesitter.lua
@@ -13,10 +13,11 @@ require('nvim-treesitter.configs').setup({
 })
 
 -- Fix for "No folds found" fold error
--- vim.api.nvim_create_autocmd({ 'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter' }, {
-  -- group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
-  -- callback = function()
-    -- vim.opt.foldmethod = 'expr'
-    -- vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
-  -- end,
--- })
+vim.api.nvim_create_autocmd({ 'BufEnter', 'BufAdd', 'BufNew', 'BufNewFile', 'BufWinEnter' }, {
+  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+  callback = function()
+    vim.opt.foldmethod = 'expr'
+    vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+    vim.opt.foldenable = false
+  end,
+})
