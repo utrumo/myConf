4c31290762559e7c0705a4f10bdbf9cba37d3bf3
Petr Devyaterikov <utrumo@gmail.com>
2023-10-05 11:50:03 +0300

    Updates config

--- nvim/lua/config/plugin-configs/efmls-configs-nvim.lua
+++ nvim/lua/config/plugin-configs/efmls-configs-nvim.lua
@@ -1,21 +1,29 @@
 local utils = require('config/keybindings/utils')
 local printInspect = utils.printInspect
 
--- Register linters and formatters per language
 local base_eslint_d_linter = require('efmls-configs.linters.eslint_d')
-local base_eslint_d_formatter = require('efmls-configs.formatters.eslint_d')
--- local base_prettier_formatter = require('efmls-configs.formatters.prettier')
-local base_prettier_d_formatter = require('efmls-configs.formatters.prettier_d')
-
 local eslint_d_linter = vim.tbl_extend('force', base_eslint_d_linter, {
   lintCommand = 'yarn eslint_d --cache' .. base_eslint_d_linter.lintCommand,
 })
+
+local base_eslint_d_formatter = require('efmls-configs.formatters.eslint_d')
 local eslint_d_formatter = vim.tbl_extend('force', base_eslint_d_formatter, {
   formatCommand = 'yarn eslint_d --cache' .. base_eslint_d_formatter.formatCommand,
 })
+
+-- local base_prettier_formatter = require('efmls-configs.formatters.prettier')
 -- local prettier_formatter = vim.tbl_extend('force', base_prettier_formatter, {
   -- formatCommand = 'yarn prettier --cache --cache-location .prettiercache' .. base_prettier_formatter.formatCommand,
 -- })
+-- local prettier_formatter = {
+  -- -- formatCanRange = true,
+  -- -- formatCommand = "yarn prettier --cache --cache-location .prettiercache --stdin --stdin-filepath '${INPUT}' ${--range-start:charStart} ${--range-end:charEnd} ${--tab-width:tabSize} ${--use-tabs:!insertSpaces}",
+  -- formatCommand = "yarn prettier --cache --cache-location .prettiercache --stdin --stdin-filepath '${INPUT}'",
+  -- formatStdin = true,
+  -- rootMarkers = { ".prettierrc", ".prettierrc.json", ".prettierrc.js", ".prettierrc.yml", ".prettierrc.yaml", ".prettierrc.json5", ".prettierrc.mjs", ".prettierrc.cjs", ".prettierrc.toml" }
+-- }
+
+local base_prettier_d_formatter = require('efmls-configs.formatters.prettier_d')
 local prettier_d_formatter = vim.tbl_extend('force', base_prettier_d_formatter, {
   formatCommand = 'yarn prettierd' .. base_prettier_d_formatter.formatCommand,
 })
@@ -25,16 +33,19 @@ local prettier_d_formatter = vim.tbl_extend('force', base_prettier_d_formatter, 
 local jsTools = {
   eslint_d_linter,
   eslint_d_formatter,
+  -- prettier_formatter,
   prettier_d_formatter,
 }
 
 local languages = {
   typescript = jsTools,
   javascript = jsTools,
+  -- jsonc = { prettier_formatter },
   jsonc = { prettier_d_formatter },
   -- lua = { stylua },
 }
 -- printInspect(prettier_formatter)
+-- printInspect(prettier_d_formatter)
 
 -- Or use the defaults provided by this plugin
 -- check doc/SUPPORTED_LIST.md for the supported languages
