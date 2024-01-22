86b767205897a1128846c8f7d565299df78a25f7
Petr Devyaterikov <utrumo@gmail.com>
2024-01-22 23:00:51 +0300

    Adds relative numbers to neo-tree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -4,6 +4,15 @@ return {
     opts = function(_, opts)
       opts.window = opts.window or {}
       opts.window.auto_expand_width = true
+
+      opts.event_handlers = opts.event_handlers or {}
+
+      table.insert(opts.event_handlers, {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.opt_local.relativenumber = true
+        end,
+      })
     end,
   },
 }
