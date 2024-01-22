d0f32e0d7ca2755ea865fe23319b685fdc0c7719
Petr Devyaterikov <utrumo@gmail.com>
2024-01-22 22:16:18 +0300

    Adds mason-tool-installer

---
+++ nvim/lua/plugins/mason-tool-installer.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = function(_, opts)
+      opts.ensure_installed = opts.ensure_installed or {}
+      -- now versions >= 4.0.0 are broken
+      table.insert(opts.ensure_installed, { "typescript-language-server", version = "4.0.0" })
+    end,
+  },
+}
