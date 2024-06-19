918aaf29e6e47f92993f45e3580080ee2940fba8
Petr Devyaterikov <utrumo@gmail.com>
2024-06-20 01:13:12 +0300

    Corrects linters

--- nvim/lua/plugins/typescript.lua
+++ nvim/lua/plugins/typescript.lua
@@ -1,13 +1,49 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    -- inlay_hints = { enabled = false },
-    servers = {
-      vtsls = {
-        settings = {
-          typescript = {
-            tsdk = "./.yarn/sdks/typescript/lib",
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      -- inlay_hints = { enabled = false },
+      -- format = {
+      --   async = true,
+      -- },
+      servers = {
+        vtsls = {
+          settings = {
+            typescript = {
+              tsdk = "./.yarn/sdks/typescript/lib",
+            },
           },
+        },
+      },
+      setup = {
+        vtsls = function(_, opts)
+          require("lazyvim.util").lsp.on_attach(function(client)
+            if client.name == "vtsls" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    ---@class ConformOpts
+    opts = {
+      formatters = {
+        prettier = {
+          command = function(self, bufnr)
+            local util = require("conform.util")
+            local cmd = util.find_executable({ ".yarn/sdks/prettier/bin/prettier.cjs" }, "")(self, bufnr)
+
+            if cmd ~= "" then
+              return cmd
+            end
+
+            -- return type of util.from_node_modules is fun(self: conform.FormatterConfig, ctx: conform.Context): string
+            ---@diagnostic disable-next-line
+            return util.from_node_modules("prettier")(self, bufnr)
+          end,
         },
       },
     },
