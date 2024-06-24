9b5a65056f11f14905357cc9eeea9f267f612382
Petr Devyaterikov <utrumo@gmail.com>
2024-06-24 15:14:13 +0300

    Updates packages

--- nvim/lua/plugins/typescript.lua
+++ nvim/lua/plugins/typescript.lua
@@ -2,7 +2,7 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      -- inlay_hints = { enabled = false },
+      inlay_hints = { enabled = false },
       -- format = {
       --   async = true,
       -- },
