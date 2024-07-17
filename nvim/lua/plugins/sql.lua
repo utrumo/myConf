c6a82dbdf658bfa36df5f2067aba6f620bca390c
Petr Devyaterikov <utrumo@gmail.com>
2024-07-17 21:07:33 +0300

    Removes overriding sql formatters

--- nvim/lua/plugins/sql.lua
+++ nvim/lua/plugins/sql.lua
@@ -7,13 +7,11 @@ return {
     "stevearc/conform.nvim",
     ---@param opts ConformOpts
     opts = function(_, opts)
-      for _, ft in ipairs({ "mysql", "plsql", "sql" }) do
-        -- table.insert(opts.formatters_by_ft[ft], "sql_formatter")
+      -- @link https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/plugins/extras/lang/sql.lua#L147
+      local sql_ft = { "sql", "mysql", "plsql" }
 
-        -- Temporary replaces sqlfluff to fix error in ~/.local/state/nvim/conform.log
-        -- 14:24:13[ERROR] Formatter 'sqlfluff' error: WARNING    Fixes for LT09 not applied, as it would result in an unparsable file. Please report this as a bug with a minimal query which demonstrates this warning.
-        --  [1 templating/parsing errors found]
-        opts.formatters_by_ft[ft] = { "sql_formatter" }
+      for _, ft in ipairs(sql_ft) do
+        table.insert(opts.formatters_by_ft[ft], "sql_formatter")
       end
 
       -- @link https://github.com/stevearc/conform.nvim/blob/master/lua/conform/formatters/sql_formatter.lua
