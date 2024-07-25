7dfdfa3b4da4ad26602b31f744a66d9eb576eee5
Petr Devyaterikov <utrumo@gmail.com>
2024-07-25 11:35:14 +0300

    Toggle codeium by hotkey

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,25 +1,54 @@
+local cmpSources = nil
+local codeiumSource = nil
+
+local getToggleCodiumToggle = function(enableNotification)
+  return function()
+    local cmp = require("cmp")
+
+    if cmpSources == nil then
+      print("Error: cmpSources == nil")
+      return
+    end
+
+    local codeumSourceIndex = nil
+
+    for i, value in ipairs(cmpSources) do
+      if value.name == "codeium" then
+        codeumSourceIndex = i
+      end
+    end
+
+    if codeumSourceIndex ~= nil then
+      codeiumSource = cmpSources[codeumSourceIndex]
+      table.remove(cmpSources, codeumSourceIndex)
+      cmp.config.sources(cmpSources)
+
+      if enableNotification then
+        print("Codeium: disabled")
+      end
+    elseif codeiumSource then
+      table.insert(cmpSources, codeiumSource)
+      cmp.config.sources(cmpSources)
+
+      if enableNotification then
+        print("Codeium: enabled")
+      end
+    end
+  end
+end
+
 return {
-  -- "nvim-cmp",
-  -- ---@param opts cmp.ConfigSchema
-  -- opts = function(_, opts)
-  --   local source = nil
-  --
-  --   for _key, value in pairs(opts.sources) do
-  --     if value.name == "codeium" then
-  --       source = value
-  --     end
-  --   end
-  --
-  --   local priority = 1
-  --
-  --   if source then
-  --     source.priority = priority
-  --   else
-  --     table.insert(opts.sources, 1, {
-  --       name = "codeium",
-  --       group_index = 1,
-  --       priority = priority,
-  --     })
-  --   end
-  -- end,
+  "nvim-cmp",
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    cmpSources = opts.sources
+    getToggleCodiumToggle(false)()
+  end,
+  keys = {
+    {
+      "<leader>ct",
+      getToggleCodiumToggle(true),
+      desc = "Codeium toggle",
+    },
+  },
 }
