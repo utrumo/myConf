c82219e11faf3e5d5fe98bcdd8595bcf963d44b7
Petr Devyaterikov <utrumo@gmail.com>
2024-11-13 09:53:24 +0300

    Updates packages

---
+++ nvim/lua/plugins/big-file.lua
@@ -0,0 +1,15 @@
+return {
+  "LunarVim/bigfile.nvim",
+  -- opts = {
+  --   features = { -- features to disable
+  --     "indent_blankline",
+  --     "illuminate",
+  --     "lsp",
+  --     "treesitter",
+  --     "syntax",
+  --     "matchparen",
+  --     "vimopts",
+  --     "filetype",
+  --   },
+  -- },
+}
